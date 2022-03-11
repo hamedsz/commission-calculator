ce64f304f98203d201fb3cf767b9890fb460ca4b
hamedsz <badmansog@gmail.com>
2022-03-11 23:53:37 +0330

    implement Private Withdraw Commission Calculator Test

--- tests/Unit/PrivateWithdrawCommissionCalculatorTest.php
+++ tests/Unit/PrivateWithdrawCommissionCalculatorTest.php
@@ -4,10 +4,54 @@ namespace Tests\Unit;
 
 use App\Entities\Operation;
 use App\Entities\Withdraw\BusinessWithdrawCommissionCalculator;
+use App\Entities\Withdraw\PrivateWithdrawCommissionCalculator;
 use PHPUnit\Framework\TestCase;
 
 class PrivateWithdrawCommissionCalculatorTest extends TestCase
 {
+
+    private function getData(){
+
+        $data = [];
+
+        $item = new Operation();
+        $item->amount = 3000000;
+        $item->date = "2016-02-19";
+        $item->currency = "JPY";
+        $data[] = [
+            'operation' => $item,
+            'result'    => 8612
+        ];
+
+        $item = new Operation();
+        $item->amount = 300.00;
+        $item->date = "2016-02-15";
+        $item->currency = "EUR";
+        $data[] = [
+            'operation' => $item,
+            'result'    => 0.00
+        ];
+
+        $item = new Operation();
+        $item->amount = 1000.00;
+        $item->date = "2016-01-10";
+        $item->currency = "EUR";
+        $data[] = [
+            'operation' => $item,
+            'result'    => 0.00
+        ];
+
+        $item = new Operation();
+        $item->amount = 100.00;
+        $item->date = "2016-01-10";
+        $item->currency = "EUR";
+        $data[] = [
+            'operation' => $item,
+            'result'    => 3.00
+        ];
+
+        return $data;
+    }
 
     /**
      * A basic test example.
@@ -16,13 +60,12 @@ class PrivateWithdrawCommissionCalculatorTest extends TestCase
      */
     public function test_is_returns_correct_data()
     {
-        for ($i=0; $i < 10; $i++){
-            $operation = new Operation();
-            $operation->amount = rand(1 , 1000000);
+        $data = $this->getData();
 
-            $obj = new BusinessWithdrawCommissionCalculator($operation);
+        foreach ($data as $item){
+            $obj = new PrivateWithdrawCommissionCalculator($item['operation']);
 
-            $this->assertTrue($obj->calculate() == ($operation->amount * 0.005));
+            $this->assertTrue($obj->calculate() === $item['result']);
         }
     }
 }
